b33512fb34fda3b0f0e968425fcdab9c516d6264
Nils Blum-Oeste <nils.blum-oeste@wooga.net>
2016-05-05 07:59:46 +0200

    Use Overloadedstrings extension FTW

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import qualified GitHub as GH
 import GitHub.Endpoints.Repos (RepoPublicity (..))
 
-import Data.Text as T
-import Data.Proxy as Proxy
-
 main :: IO ()
 main = do
-  let nameText = T.pack "nblumoe"
-  let name =  GH.mkName Proxy nameText
-  possibleUser <- GH.executeRequest' $ (GH.userReposR name RepoPublicityPublic Nothing)
+  possibleUser <- GH.executeRequest' $ GH.userReposR "nblumoe"  RepoPublicityPublic Nothing
   print possibleUser
