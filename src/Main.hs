e90d295313650e5000f8cc23788e305b9940ee66
Nils Blum-Oeste <nils.blum-oeste@wooga.net>
2016-05-08 18:41:47 +0200

    Fetch open PRs for a team
    Pretty dirty, hacky implementation. Haskell fluency apparently takes
    more than an afternoon. ;)

--- src/Main.hs
+++ src/Main.hs
@@ -4,7 +4,6 @@ import System.Environment (lookupEnv, getArgs)
 import Data.String (fromString)
 
 import qualified GitHub
--- import           GitHub.Endpoints.Repos (RepoPublicity (..))
 import Data.Text as T
 import Data.Text.IO as TIO
 import Data.String  (fromString)
@@ -16,10 +15,6 @@ getAuth = do
   token <- lookupEnv "GITHUB_TOKEN"
   pure (GitHub.OAuth . fromString <$> token)
 
--- fetch teams of organisation
--- fetch team repos and filter by open_issues_count
--- fetch open pull requests for all relevant repos
-
 teamNameMatches :: Text -> GitHub.SimpleTeam -> Bool
 teamNameMatches team_name team =
     (GitHub.untagName $ GitHub.simpleTeamSlug team) == team_name
@@ -28,7 +23,10 @@ findTeamId :: [Char] -> (V.Vector GitHub.SimpleTeam) -> Maybe Int
 findTeamId team_name teams =
   case V.find (teamNameMatches (T.pack team_name)) teams of
     Nothing -> Nothing
-    Just t -> Just $ GitHub.untagId $ GitHub.simpleTeamId t
+    Just t ->  Just $ GitHub.untagId $ GitHub.simpleTeamId t
+
+fetchPRs auth org_name repo_name =
+  GitHub.executeRequestMaybe auth $ GitHub.pullRequestsForR (GitHub.mkOwnerName org_name) (GitHub.mkRepoName repo_name) Nothing Nothing
 
 main :: IO ()
 main = do
@@ -36,18 +34,24 @@ main = do
   auth <- getAuth
   possibleTeams <- GitHub.executeRequestMaybe auth $ GitHub.teamsOfR (GitHub.mkOrganizationName $ T.pack org_name) Nothing
   let team_id = case possibleTeams of
-              Left error   -> Nothing
-              Right teams  -> findTeamId team_name teams
+                  Right teams  -> findTeamId team_name teams
+                  Left error   -> Nothing
 
   repos <- case team_id of
-        Nothing -> error "No repos found"
-        Just id -> GitHub.executeRequestMaybe auth $ GitHub.listTeamReposR (GitHub.mkTeamId id) Nothing
+    Just id -> GitHub.executeRequestMaybe auth $ GitHub.listTeamReposR (GitHub.mkTeamId id) Nothing
+    Nothing -> error "No repos found"
 
-  TIO.putStrLn $ either (("No repos found" <>) . tshow)
-                        (foldMap ((<> "\n") .formatRepo))
-                        repos
+  pull_requests <- case repos of
+                        Left _ -> error "foooo"
+                        Right rs  -> (V.mapM ((fetchPRs auth (T.pack org_name)) . GitHub.untagName .GitHub.repoName) rs)
+
+  V.mapM_ (\r ->
+             TIO.putStrLn $ either (("Error" <>) . tshow)
+                                   (foldMap ((<> "\n") . formatPR))
+                                   r) pull_requests
 
 formatRepo = GitHub.untagName . GitHub.repoName
+formatPR = GitHub.simplePullRequestTitle
 
 tshow :: Show a => a -> Text
 tshow = T.pack . show
